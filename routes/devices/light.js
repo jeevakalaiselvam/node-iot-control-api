76b98fe87179416cec7caa8a0c641c9d460568db
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-29 14:06:28 +0530

    Post working for light

--- routes/devices/light.js
+++ routes/devices/light.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
 // Light structure is imported at the top of the file
 const lightStructure = require("./lightStructure.js");
@@ -27,6 +28,30 @@ router.get("/actions", function (req, res, next) {
     let info = lightStructure.actions;
     res.json(info);
 });
+
+// Function to run if the user sends a PUT request
+router.put(
+    ["/", "/actions/fade"],
+    [
+        check("level").isNumeric().isLength({ min: 0, max: 100 }),
+        check("duration").isNumeric().optional().isLength({ min: 0 }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        let level = req.body.level;
+        let duration;
+        if (req.body.duration) {
+            duration = req.body.duration;
+        } else {
+            duration = 500;
+        }
+        let message = `success: level to ${level} over ${duration} milliseconds`;
+        res.json({ message: message });
+    }
+);
 
 // Export route so it is available to import
 module.exports = router;
