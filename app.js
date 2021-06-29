de2b6de99c4ba8cbb313632f871c81615fb8dc87
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-29 15:53:16 +0530

    Simple JSON Web token

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+// Import library for working with JWT tokens
+const jwt = require("jwt-simple");
+// import our config file and get the secret value
+const config = require("./config/config");
+const secret = config.secret;
 
 // Import path and file system libraries for importing our route files
 const path = require("path");
@@ -12,6 +17,7 @@ const createError = require("http-errors");
 //Declare routes
 let index = require("./routes/index");
 let light = require("./routes/devices/light");
+let checkIn = require("./routes/check-in");
 
 // Tell express to enable url encoding
 app.use(express.urlencoded({ extended: true }));
@@ -19,11 +25,31 @@ app.use(express.json());
 app.use(morgan("common"));
 app.set("view engine", "ejs");
 
-// Provide data for root
+//CHECKIN IN USING JSON WEB TOKEN
+// Tell Express to use our index module for root URL
 app.use("/", index);
-
-// Provide the data for light
+app.use("/check-in", checkIn);
 app.use("/devices/light", light);
+app.use("/devices/restricted-light", isCheckedIn, light);
+
+// Check if the requesting client has checked in
+function isCheckedIn(req, res, next) {
+    // Check that authorization header was sent
+    if (req.headers.authorization) {
+        // Get token from "Bearer: Token" string
+        let token = req.headers.authorization.split(" ")[1];
+        // Try decoding the client's JWT using the server secret
+        try {
+            req._guest = jwt.decode(token, secret);
+        } catch {
+            res.status(403).json({ error: "Token is not valid." });
+        }
+        // If the decoded object has a name protected route can be used
+        if (req._guest.name) return next();
+    }
+    // If no authorization header or guest has no name return a 403 error
+    res.status(403).json({ error: "Please check-in to recieve a token." });
+}
 
 // Catch any 404 and send to handler
 app.use(function (req, res, next) {
